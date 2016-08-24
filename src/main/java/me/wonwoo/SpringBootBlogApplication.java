82d9fbe5f6d4a7025fb350caf04411ef669ba3f3
wonwoo <aoruqjfu@gmail.com>
2016-08-24 23:37:42 +0900

    카페인 캐시 추가

--- src/main/java/me/wonwoo/SpringBootBlogApplication.java
+++ src/main/java/me/wonwoo/SpringBootBlogApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -36,6 +37,7 @@ import java.util.Collections;
 @EntityScan(basePackageClasses = {SpringBootBlogApplication.class, Jsr310JpaConverters.class })
 @EnableConfigurationProperties(GitProperties.class)
 @EnableJpaAuditing
+@EnableCaching
 public class SpringBootBlogApplication {
 
 	public static void main(String[] args) {
