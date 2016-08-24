615b58ecd514dbd124b9d7fa50d1206e7b308852
wonwoo <aoruqjfu@gmail.com>
2016-08-24 14:31:40 +0900

    페이징 처리

--- src/main/java/me/wonwoo/web/IndexController.java
+++ src/main/java/me/wonwoo/web/IndexController.java
@@ -2,6 +2,9 @@ package me.wonwoo.web;
 
 import lombok.RequiredArgsConstructor;
 import me.wonwoo.domain.repository.PostRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +19,8 @@ public class IndexController {
   private final PostRepository postRepository;
 
   @GetMapping("/")
-  public String home(Model model){
-    model.addAttribute("posts", postRepository.findAll());
+  public String home(Model model, @PageableDefault(size = 5, sort = "regDate" ,direction = Sort.Direction.DESC)  Pageable pageable){
+    model.addAttribute("posts", postRepository.findAll(pageable));
     return "index";
   }
 
