c9cb27b642816d56788a430a0311002bc841461f
wonwoo <aoruqjfu@gmail.com>
2016-08-29 11:20:13 +0900

    test test? 추가

--- src/test/java/me/wonwoo/SpringBootBlogApplicationTests.java
+++ src/test/java/me/wonwoo/SpringBootBlogApplicationTests.java
@@ -5,12 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SpringBootBlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertThat("1").isEqualTo("1");
 	}
 
 }
