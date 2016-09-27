9d693999f929170a09cc91ae949c3802d49c78a0
wonwoo <aoruqjfu@gmail.com>
2016-09-27 15:56:08 +0900

    하이버네이트 풀 서치

--- src/main/java/me/wonwoo/wordpress/domain/WpPosts.java
+++ src/main/java/me/wonwoo/wordpress/domain/WpPosts.java
@@ -1,6 +1,8 @@
 package me.wonwoo.wordpress.domain;
 
 import lombok.Data;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,20 +12,25 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Indexed
 public class WpPosts {
     @Id @GeneratedValue
     @Column(name = "ID")
     private Long id;
+    @Field
     @Column(name = "post_title")
     private String postTitle;
     @Column(name = "post_author")
     private String postAuthor;
     @Column(name = "post_content")
+    @Field
     private String postContent;
     @Column(name = "post_date", columnDefinition = "datetime")
     private LocalDateTime postDate;
     @Column(name = "post_type")
+    @Field
     private String postType;
+    @Field
     @Column(name = "post_status")
     private String postStatus;
     @Column(name ="post_content_filtered")
--- src/main/java/me/wonwoo/wordpress/service/WpPostsService.java
+++ src/main/java/me/wonwoo/wordpress/service/WpPostsService.java
@@ -5,16 +5,20 @@ package me.wonwoo.wordpress.service;
 import lombok.RequiredArgsConstructor;
 import me.wonwoo.wordpress.domain.WpPosts;
 import me.wonwoo.wordpress.repostiry.WpPostsRepository;
-import org.pegdown.PegDownProcessor;
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.BooleanJunction;
+import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
-import static org.springframework.data.domain.ExampleMatcher.matching;
+import java.util.Optional;
 
 @Service
 @Transactional(transactionManager ="wordPressTransactionManager")
@@ -23,15 +27,48 @@ public class WpPostsService {
 
     private final WpPostsRepository wpPostsRepository;
 
-    public Page<WpPosts> findByPosts(String q, Pageable pageable){
-        WpPosts wpPosts = new WpPosts();
-        wpPosts.setPostTitle(q);
-        wpPosts.setPostStatus("publish");
-        wpPosts.setPostType("post");
-        Example<WpPosts> wpPostsExample = Example.of(wpPosts,
-                matching()
-                    .withMatcher("postTitle", ExampleMatcher.GenericPropertyMatcher::contains));
-        return wpPostsRepository.findAll(wpPostsExample, pageable);
+    @PersistenceContext(unitName = "wordPress")
+    private EntityManager entityManager;
+
+    public PageImpl findByPosts(String q, Pageable pageable){
+
+        FullTextEntityManager fullTextEntityManager =
+                Search.getFullTextEntityManager(entityManager);
+
+        QueryBuilder queryBuilder =
+                fullTextEntityManager.getSearchFactory()
+                        .buildQueryBuilder().forEntity(WpPosts.class).get();
+
+        final BooleanJunction<BooleanJunction> outer = queryBuilder.bool();
+        outer.must(queryBuilder
+                .keyword()
+                .onFields("postTitle", "postContent")
+                .matching(q)
+                .createQuery());
+
+        Optional.of("publish")
+                .map(f -> queryBuilder.range().onField("postStatus").above(f).createQuery())
+                .ifPresent(outer::must);
+
+        Optional.of("post")
+                .map(f -> queryBuilder.range().onField("postType").above(f).createQuery())
+                .ifPresent(outer::must);
+
+        FullTextQuery jpaQuery =
+                fullTextEntityManager.createFullTextQuery(outer.createQuery(), WpPosts.class);
+        List results = jpaQuery.getResultList();
+        return new PageImpl<>(results,pageable, jpaQuery.getResultSize());
+
+//        return results;
+//
+//        WpPosts wpPosts = new WpPosts();
+//        wpPosts.setPostTitle(q);
+//        wpPosts.setPostStatus("publish");
+//        wpPosts.setPostType("post");
+//        Example<WpPosts> wpPostsExample = Example.of(wpPosts,
+//                matching()
+//                    .withMatcher("postTitle", ExampleMatcher.GenericPropertyMatcher::contains));
+//        return wpPostsRepository.findAll(wpPostsExample, pageable);
     }
 
     public WpPosts findByPost(Long id) {
