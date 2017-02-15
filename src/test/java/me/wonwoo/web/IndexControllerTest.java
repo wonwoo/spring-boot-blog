41bd3e76e18c8a02274d41570f6ad2abf8ea059b
wonwoo <aoruqjfu@gmail.com>
2017-02-16 00:21:24 +0900

    index test 수정

--- src/test/java/me/wonwoo/web/IndexControllerTest.java
+++ src/test/java/me/wonwoo/web/IndexControllerTest.java
@@ -1,29 +1,74 @@
 package me.wonwoo.web;
 
+import me.wonwoo.config.PostProperties;
+import me.wonwoo.domain.model.Post;
+import me.wonwoo.domain.model.User;
+import me.wonwoo.domain.repository.CategoryRepository;
+import me.wonwoo.domain.repository.PostRepository;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by wonwoo on 2016. 8. 29..
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IndexControllerTest {
+@WebMvcTest(IndexController.class)
+@EnableSpringDataWebSupport
+public class IndexControllerTest extends AbstractControllerTests {
+
+  @MockBean
+  private PostRepository postRepository;
+
+  @SpyBean
+  private PostProperties postProperties;
+
+  @MockBean
+  private CategoryRepository categoryRepository;
 
   @Autowired
-  private TestRestTemplate restTemplate;
+  private MockMvc mockMvc;
 
   @Test
-  public void homeTest() {
-    ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
-    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+  public void homeTest() throws Exception {
+    postProperties.setFull(true);
+
+    Post post = new Post("post test", "Y");
+    post.setContent("text content");
+    post.setRegDate(LocalDateTime.now());
+    post.setCode("text content");
+    post.setUser(new User(null, "wonwoo", null, null, null, true));
+    given(postRepository.findAll(any(), any(Pageable.class)))
+      .willReturn(new PageImpl<>(Collections.singletonList(post)));
+    final MvcResult mvcResult = mockMvc.perform(get("/"))
+      .andExpect(status().isOk())
+      .andReturn();
+
+    Page<Post> posts = (Page<Post>) mvcResult.getModelAndView().getModel().get("posts");
+    boolean isFull = (boolean) mvcResult.getModelAndView().getModel().get("show");
+    assertThat(posts.getContent().get(0).getContent()).isEqualTo("text content");
+    assertThat(posts.getContent().get(0).getTitle()).isEqualTo("post test");
+    assertThat(posts.getContent().get(0).getCode()).isEqualTo("text content");
+    assertThat(posts.getContent().get(0).getYn()).isEqualTo("Y");
+    assertThat(isFull).isTrue();
+    verify(postRepository, atLeastOnce()).findAll(any(), any(Pageable.class));
   }
 }
