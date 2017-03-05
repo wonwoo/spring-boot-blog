802023d5b208334004821e73b5efa16a82a4d16a
wonwoo <aoruqjfu@gmail.com>
2017-03-05 20:52:10 +0900

    github username properties 으로 변경

--- src/main/java/me/wonwoo/web/GithubPageController.java
+++ src/main/java/me/wonwoo/web/GithubPageController.java
@@ -5,6 +5,7 @@ import me.wonwoo.support.github.GithubClient;
 import me.wonwoo.support.github.page.GithubPage;
 import org.apache.commons.codec.binary.Base64;
 import org.pegdown.PegDownProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,25 +23,25 @@ import static java.util.stream.Collectors.toList;
 @Navigation(Section.GITHUB)
 public class GithubPageController {
 
-    private final GithubClient githubClient;
-    private final PegDownProcessor pegDownProcessor;
+  private final GithubClient githubClient;
+  private final PegDownProcessor pegDownProcessor;
 
-    @GetMapping
-    public String githubPages(Model model){
-        List<GithubPage> githubPages = Arrays.asList(githubClient.sendRequest("repos/wonwoo/github/contents/_post", GithubPage[].class));
-        model.addAttribute("githubPages", githubPages
-          .stream()
-          .filter(githubPage -> githubPage.getName().contains(".md"))
-          .peek(githubPage -> githubPage.setName(githubPage.getName().replace(".md", "")))
-          .collect(toList()));
-        return "github/list";
-    }
+  @GetMapping
+  public String githubPages(Model model, @Value("${github.username:wonwoo}") String githubName) {
+    List<GithubPage> githubPages = Arrays.asList(githubClient.sendRequest("repos/" + githubName + "/github/contents/_post", GithubPage[].class));
+    model.addAttribute("githubPages", githubPages
+      .stream()
+      .filter(githubPage -> githubPage.getName().contains(".md"))
+      .peek(githubPage -> githubPage.setName(githubPage.getName().replace(".md", "")))
+      .collect(toList()));
+    return "github/list";
+  }
 
-    @GetMapping("/{name}")
-    public String githubPage(@PathVariable String name, Model model){
-        GithubPage githubPage = githubClient.sendRequest("repos/wonwoo/github/contents/_post/" + name + ".md", GithubPage.class);
-        githubPage.setContent(pegDownProcessor.markdownToHtml(new String(Base64.decodeBase64(githubPage.getContent()))));
-        model.addAttribute("githubPage", githubPage);
-        return "github/page";
-    }
+  @GetMapping("/{name}")
+  public String githubPage(@PathVariable String name, Model model, @Value("${github.username:wonwoo}") String githubName) {
+    GithubPage githubPage = githubClient.sendRequest("repos/" + githubName + "/github/contents/_post/" + name + ".md", GithubPage.class);
+    githubPage.setContent(pegDownProcessor.markdownToHtml(new String(Base64.decodeBase64(githubPage.getContent()))));
+    model.addAttribute("githubPage", githubPage);
+    return "github/page";
+  }
 }
