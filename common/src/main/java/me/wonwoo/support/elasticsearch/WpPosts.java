6866cee8b55b8fe9cce4ca1585cd53d14e1fdfeb
wonwoo <aoruqjfu@gmail.com>
2017-08-12 22:02:20 +0900

    엘라스틱 index 제거

--- common/src/main/java/me/wonwoo/support/elasticsearch/WpPosts.java
+++ common/src/main/java/me/wonwoo/support/elasticsearch/WpPosts.java
@@ -8,6 +8,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 @Data
 @Document(indexName = "wordpress", type = "post", shards = 5, replicas = 0, refreshInterval = "-1")
@@ -17,15 +18,18 @@ public class WpPosts {
     private Integer id;
 
     @JsonProperty("post_title")
+    @Field(analyzer = "simple" , store = true)
     private String postTitle;
 
     @JsonProperty("post_author")
     private Integer postAuthor;
 
+    @Field(analyzer = "fulltext" , store = true)
     @JsonProperty("post_content")
     private String postContent;
 
     @JsonProperty("post_date")
+    @Field(store = true)
     private LocalDateTime postDate;
 
     @JsonProperty("post_type")
@@ -35,8 +39,10 @@ public class WpPosts {
     private String postStatus;
 
     @JsonProperty("post_content_filtered")
+    @Field(analyzer = "fulltext" , store = true)
     private String postContentFiltered;
 
+    @Field(analyzer = "fulltext" , store = true)
     private String highlightedContent;
 
     private String tableOfContent;
