6557233e67269c24eae7c387dba90f582e0a6f4d
wonwoo <aoruqjfu@gmail.com>
2018-12-23 19:15:36 +0900

    Polish

--- common/src/test/java/me/wonwoo/domain/repository/impl/PostRepositoryImplTests.java
+++ common/src/test/java/me/wonwoo/domain/repository/impl/PostRepositoryImplTests.java
@@ -1,8 +1,12 @@
 package me.wonwoo.domain.repository.impl;
 
-import java.util.Arrays;
-import java.util.Collections;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import me.wonwoo.domain.model.Category;
+import me.wonwoo.domain.model.Post;
+import me.wonwoo.domain.model.User;
+import me.wonwoo.domain.repository.PostRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +15,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import me.wonwoo.domain.model.Category;
-import me.wonwoo.domain.model.CategoryPost;
-import me.wonwoo.domain.model.Post;
-import me.wonwoo.domain.model.User;
-import me.wonwoo.domain.repository.PostRepository;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -38,15 +34,13 @@ public class PostRepositoryImplTests {
 				"https://avatars.githubusercontent.com/u/747472?v=3", "password", true));
 		final Post post = this.testEntityManager.persist(new Post("test title", "test content", "test content", "Y", user, Arrays.asList("spring", "jpa")));
 		final Category category = this.testEntityManager.persist(new Category("spring"));
-		CategoryPost categoryPost = testEntityManager.persist(new CategoryPost(category, post));
-		post.setCategoryPost(Collections.singletonList(categoryPost));
-		Page<Post> posts = postRepository.findByPostsForCategory(category.getId(), new PageRequest(0, 1));
+		Page<Post> posts = postRepository.findByPostsForCategory(category.getId(), PageRequest.of(0, 1));
 		assertThat(posts.getTotalElements()).isEqualTo(1);
 		assertThat(posts.getContent().get(0).getTitle()).isEqualTo("test title");
 		assertThat(posts.getContent().get(0).getContent()).isEqualTo("test content");
 		assertThat(posts.getContent().get(0).getCode()).isEqualTo("test content");
 		assertThat(posts.getContent().get(0).getYn()).isEqualTo("Y");
-		assertThat(posts.getContent().get(0).getCategoryPost().get(0).getId()).isEqualTo(categoryPost.getId());
+		assertThat(posts.getContent().get(0).getCategory()).isEqualTo(category);
 	}
 
 	@Test
@@ -57,8 +51,7 @@ public class PostRepositoryImplTests {
 				"https://avatars.githubusercontent.com/u/747472?v=3", "password", true));
 		final Post post = this.testEntityManager.persist(new Post("test title", "test content", "test content", "Y", user, Arrays.asList("spring", "jpa")));
 		final Category category = this.testEntityManager.persist(new Category("spring"));
-		testEntityManager.persist(new CategoryPost(category, post));
-		Page<Post> posts = postRepository.findByPostsForTag("spring", new PageRequest(0, 1));
+		Page<Post> posts = postRepository.findByPostsForTag("spring", PageRequest.of(0, 1));
 		assertThat(posts.getTotalElements()).isEqualTo(1);
 		assertThat(posts.getContent().get(0).getTitle()).isEqualTo("test title");
 		assertThat(posts.getContent().get(0).getContent()).isEqualTo("test content");
