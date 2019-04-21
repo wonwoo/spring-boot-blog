02ac2ee0f16c02aacbb2f4d17dcff3b44d3c29ce
fidel <aoruqjfu@gmail.com>
2019-04-21 22:12:36 +0900

    pull indexing 추가

---
+++ indexer/src/main/java/me/wonwoo/blog/BlogFullIndexer.java
@@ -0,0 +1,50 @@
+package me.wonwoo.blog;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.wonwoo.Indexer;
+import me.wonwoo.domain.model.WpPost;
+import me.wonwoo.domain.repository.WpPostsRepository;
+import me.wonwoo.support.elasticsearch.PostElasticSearchService;
+import me.wonwoo.support.elasticsearch.WpPosts;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BlogFullIndexer implements Indexer<WpPost> {
+
+    private final WpPostsRepository wpPostsRepository;
+    private final PostElasticSearchService postElasticSearchService;
+
+    @Override
+    public Iterable<WpPost> indexItems() {
+        postElasticSearchService.reset(WpPosts.class);
+        return wpPostsRepository.findByPostTypeAndPostStatus("post", "publish");
+    }
+
+    @Override
+    public void indexItem(WpPost index) {
+        WpPosts wpPosts = new WpPosts();
+        wpPosts.setPostContent(index.getPostContent());
+        wpPosts.setPostTitle(index.getPostTitle());
+        wpPosts.setId(index.getId());
+        wpPosts.setPostContentFiltered(index.getPostContentFiltered());
+        wpPosts.setPostType(index.getPostType());
+        wpPosts.setPostStatus(index.getPostStatus());
+        wpPosts.setPostAuthor(index.getPostAuthor());
+        wpPosts.setPostDate(index.getPostDate());
+        postElasticSearchService.save(wpPosts);
+    }
+
+    @Override
+    public void save(WpPost index) {
+
+    }
+
+    @Override
+    public void error(WpPost index, Throwable e) {
+        logger.error("BlogIndexer error ", e);
+        logger.error("id : {}, title : {} ", index.getId(), index.getPostTitle());
+    }
+}
